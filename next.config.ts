6b8b1a8942972cacf8d18a87acfb89c11edddaae
Jaiprakash L <jayprakash_88@yahoo.co.in>
2026-05-24 18:35:13 +0530

    Change output configuration to ignore ESLint during builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: "eslint.ignoreDuringBuilds",
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
